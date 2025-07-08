30c0845d2d8a7cb9620bbec00fd787a239498d0d
laoniu-meow <laoniu.meow@gmail.com>
2025-07-08 22:00:20 +0800

    Temporary remove the backend.tf

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "ce10-laoniu-bucket-255945442255" # MUST EXIST beforehand
-    key    = "dev/terraform.tfstate"           # Path inside the bucket
-    region = "ap-southeast-1"
-  }
-}

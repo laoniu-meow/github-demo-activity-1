0fc872811f6e992974df1dc62094668be28ac1df
laoniu-meow <laoniu.meow@gmail.com>
2025-07-08 21:40:18 +0800

    Modifier the Terraform code

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-southeast-1"  # Change as needed
+  region = "ap-southeast-1" # Change as needed
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "arn:aws:iam::255945442255:user/laoniu.meow"  # MUST be globally unique
+resource "aws_s3_bucket" "example-bucket" {
+  bucket        = "ce10-laoniu-bucket-255945442255" # MUST be globally unique
   force_destroy = true
 
   tags = {
